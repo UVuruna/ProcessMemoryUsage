29c1f683a399fdb4bf02f0028662e5aa1ba00de3
UVuruna <vurunayas@gmail.com>
2024-09-23 14:12:52 +0200

    beta

--- MemoryUsage.py
+++ MemoryUsage.py
@@ -8,26 +8,33 @@ root.title("Top Memory Processes")
 
 UNITS = True
 RESOURCE = 'memory_info'
-fontHEAD = defineFont('Arial',14)
-FONT = defineFont('Arial',13)
+fontHEAD = defineFont('Arial', 14)
+FONT = defineFont('Arial', 13)
+
 
 def heading_Main(memoryUsage):
-    return 'Current usage {:,.{}f} {}: {:.1f}%'.format(memoryUsage,decimal,unit,memoryUsage*100/memory,decimal)
+    return 'Current usage {:,.{}f} {}: {:.1f}%'.format(memoryUsage, decimal, unit, memoryUsage*100/memory, decimal)
+
 
 def START():
-    global memory,refreshRate,removeRate,currHead,currFrame,lenCurr,highHead,highFrame,lenHigh,highData,unit,unitValue,decimal
-    GetSettingRETURN = GetSetting(root,SettingScreen,MainScreen,unitsIndex,fontHEAD,FONT,red,blue,UNITS)
-    PC,refreshRate,removeRate,currHead,currFrame,lenCurr,highHead,highFrame,lenHigh,highData,unit,unitValue,decimal = GetSettingRETURN
+    global memory, refreshRate, removeRate, currHead, currFrame, lenCurr, highHead, highFrame, lenHigh, highData, unit, unitValue, decimal
+    GetSettingRETURN = GetSetting(
+        root, SettingScreen, MainScreen, unitsIndex, fontHEAD, FONT, red, blue, UNITS)
+    PC, refreshRate, removeRate, currHead, currFrame, lenCurr, highHead, highFrame, lenHigh, highData, unit, unitValue, decimal = GetSettingRETURN
     memory = PC*(1024**3/unitsIndex[unit])
-    
+
     update_process()
 
+
 def update_process():
-    UpdateProcess(RESOURCE,currFrame,lenCurr,highFrame,lenHigh,highData,removeRate,decimal,unit,unitValue,heading_Main,currHead)
-    highHead.config(text=heading_High(unit,decimal))
-    
+    UpdateProcess(RESOURCE, currFrame, lenCurr, highFrame, lenHigh, highData,
+                  removeRate, decimal, unit, unitValue, heading_Main, currHead)
+    highHead.config(text=heading_High(unit, decimal))
+
     root.after(refreshRate, update_process)
 
-SettingScreen = SettingScreenCreate(root,START, FONT,bgColor, unitsIndex,UNITS)
+
+SettingScreen = SettingScreenCreate(
+    root, START, FONT, bgColor, unitsIndex, UNITS)
 
 root.mainloop()
